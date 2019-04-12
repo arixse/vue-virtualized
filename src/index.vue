c92009389d5d08d9d16218a3236cf9f62c0d1a4f
fusheng <fusheng@netposa.com>
2019-04-12 18:45:52 +0800

    fix

--- src/index.vue
+++ src/index.vue
@@ -1,1 +1,42 @@
 
+<template>
+  <div class="vue-virtualized-container">
+    <slot v-for="item in items" :item="item"></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "VueVirtualized",
+  data() {
+    return {};
+  },
+  props: {
+    containerHeight: {
+      type: Number
+    },
+    itemHeight: {
+      type: Number
+    },
+    records: {
+      type: Array,
+      default: () => []
+    }
+  },
+  computed: {
+    containerHeight() {
+      return this.options.containerHeight;
+    },
+    itemHeight() {
+      return this.options.itemHeight;
+    },
+    virtualizedCount() {
+      let count = Math.ceil(this.containerHeight / this.itemHeight) + 1;
+    }
+  },
+  methods: {
+    handleScroll(scrollTop) {}
+  }
+};
+</script>
+<style>
+</style>
