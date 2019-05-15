41538f9d4e39d70f61c6480ed4537d8b21055817
fusheng <fusheng@netposa.com>
2019-05-15 17:24:36 +0800

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	entry: path.join(__dirname, "test", "index"),
 	output: {
 		path: path.resolve(__dirname, "docs"),
-		publicPath: "/docs/",
+		publicPath: "",
 		filename: "bundle.js",
 		chunkFilename: "[name].js"
 	},
